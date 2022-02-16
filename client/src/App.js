8b2b3527dbb73c5376993089f34e92a89845d8b3
Victor Ajagunna <victor-ajagunna@hotmail.com>
2022-02-16 22:47:24 +0100

    Some more changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,12 @@
 
 import './App.css';
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
   return (
-    <div className="App">
-      hello from us
-      <p>This is it! Hello from Victor</p>
-      igor i added another p
-      brand new 
-      ffffffffffff
-
-      Victor signing in
-      <h4>Hola</h4>
-      1219ea3a5d5b1359890f168266a1d7b1b3e23b6b
-    </div>
+  <div className='App'>
+    <Navbar />
+  </div>
   );
 }
 
--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import './Sidebar';
 
+
 function Sidebar() {
+    // const({})
   return (
     <div>Sidebar</div>
   )
