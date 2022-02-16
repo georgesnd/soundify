a0d37548491241e9ab7c29c1bdc1e33a335d40fa
George <michaelsunday10@yahoo.com>
2022-02-16 14:43:50 +0100

    brand new

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      hello from us
+      <p>This is it! Hello from Victor</p>
+      igor i added another p
+    </div>
+  );
+}
+
+export default App;
