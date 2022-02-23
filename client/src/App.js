783bbbe049f7179683a6d19995909e5d836a9faf
George <michaelsunday10@yahoo.com>
2022-02-23 12:05:27 +0100

    new changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './App.css';
-import Footer from './components/Footer/Footer';
-import Header from './components/Header';
-import Home from './components/HomePage/Home';
+// import Footer from './components/Footer/Footer';
+// import Header from './components/Header';
+// import Home from './components/HomePage/Home';
+import StartPage from './components/StartPage';
 function App() {
   return (
     <div className="App">
-      <Header/>
+      {/* <Header/>
       <Home/>
-      <Footer/>
+      <Footer/> */}
+      
+      <StartPage/>
     </div>
   );
 }
---
+++ client/src/components/StartPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Footer from './Footer/Footer'
+import Header from './Header'
+import Home from './HomePage/Home'
+
+export default function StartPage() {
+  return (
+    <div>
+        <Header/>
+        <Home/>
+        <Footer/>
+    </div>
+  )
+}
