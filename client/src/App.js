49cff6a38df7aae04abc51ff2714c24e9088010b
Victor Ajagunna <victor-ajagunna@hotmail.com>
2022-02-23 12:26:30 +0100

    clarified mistakes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,8 @@
 // import MainInterface from './components/igor-components/MainInterface';
 import "./App.css";
-import Fakeheader from "./components/igor-components/Fakeheader";
-import Playlists from "./components/igor-components/playlists/Playlists";
-import Support from "./components/HomePage/support/Support";
+
 // import "bootstrap/dist/bootstrap/css/bootstrap.min.css"
-import Login from "./Login";
-import Dashboard from "./Dashboard";
+
 import LeftMenu from "./components/HomePage/LeftMenu";
 import MainContainer from "./components/HomePage/MainContainer";
 import RightMenu from "./components/HomePage/RightMenu";
