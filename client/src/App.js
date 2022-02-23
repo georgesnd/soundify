d1d60ee8b0daf3d8b483bb2328abe63cba6bfcd9
Igor <machold.igor@gmail.com>
2022-02-23 13:34:58 +0100

    new updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,27 @@
 // import MainInterface from './components/igor-components/MainInterface';
-import "./App.css";
-import Fakeheader from "./components/igor-components/Fakeheader";
+// import "./App.css";
+
 import Playlists from "./components/igor-components/playlists/Playlists";
 import Support from "./components/HomePage/support/Support";
 // import "bootstrap/dist/bootstrap/css/bootstrap.min.css"
-import Login from "./Login";
-import Dashboard from "./Dashboard";
+import Background from "./components/HomePage/Background";
+
+// import AllInOne from "./components/HomePage/AllInOne";
 import LeftMenu from "./components/HomePage/LeftMenu";
 import MainContainer from "./components/HomePage/MainContainer";
 import RightMenu from "./components/HomePage/RightMenu";
-
-
 
 function App() {
   return (
     <div className="App">
       <LeftMenu />
       <MainContainer />
-
       <RightMenu />
+      <Background />
 
+      {/* <AllInOne /> */}
 
-   
-      {/* <Fakeheader /> */}
-
-      {/* <MainInterface />
-      <Playlists /> */}
-
-      {/* <Support /> */}
-
-   
-
-      <div className="background"></div>
+      {/* <div className="background"></div> */}
     </div>
   );
 }
